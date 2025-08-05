c7a0de60e3ebead29c21bf4e16ebcbf1c842675e
vvartakMoJ <vidyanand.vartak@digital.justice.gov.uk>
2025-08-05 16:29:57 +0100

    Checkstyle related changes.

--- claims-data/service/src/main/java/uk/gov/justice/laa/dstew/payments/claimsdata/aop/JaversAuditingAspect.java
+++ claims-data/service/src/main/java/uk/gov/justice/laa/dstew/payments/claimsdata/aop/JaversAuditingAspect.java
@@ -27,6 +27,14 @@ public class JaversAuditingAspect {
   //TODO: replace with the actual user ID/name when available, as per DSTEW-88
   private static final String API_USER = "api-user";
 
+  /**
+   * Audits the save operation performed on an entity within the specified repository package.
+   * This is run after the successful execution of the save method in any of the classes in the specified repository package.
+   * It commits the saved entity into Javer for tracking changes.
+   *
+   * @param joinPoint the join point providing reflective access to the intercepted method
+   * @param result the result of the save operation, representing the saved entity
+   */
   @AfterReturning(pointcut = "execution(* uk.gov.justice.laa.dstew.payments.claimsdata.repository.*.save(..))", returning = "result")
   public void auditSave(JoinPoint joinPoint, Object result) {
     if (result != null) {
@@ -34,6 +42,14 @@ public class JaversAuditingAspect {
     }
   }
 
+  /**
+   * Audits the delete operation performed on an entity within the specified repository package.
+   * This is run after the successful execution of the deleteById method in any of the classes in the specified repository package.
+   * It commits the deleted entity into Javer for tracking changes.
+   *
+   * @param joinPoint the join point providing reflective access to the intercepted method
+   * @param id Id of the entity being deleted, used to fetch the data to be audited, before it gets deleted.
+   */
   @Before("execution(* uk.gov.justice.laa.dstew.payments.claimsdata.repository.*.deleteById(..)) && args(id)")
   public void auditDelete(JoinPoint joinPoint, Object id) {
     // You need to load the entity before deletion to audit it
