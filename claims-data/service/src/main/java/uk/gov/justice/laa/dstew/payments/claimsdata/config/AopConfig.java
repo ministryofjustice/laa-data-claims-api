3cab2d8ab026967cadc8bfd6e445e71e1b1e02ff
vvartakMoJ <vidyanand.vartak@digital.justice.gov.uk>
2025-08-06 14:23:08 +0100

    Chore/dstew 247 javers (#26)
    Implement javers to create the javers tables and store history of data changes in them.
    Use AOP to apply javers auditing to all repositories in the project.
    Added SecAction to ingress annotations (via values files) to allow PUT, PATCH and DELETE operations.

---
+++ claims-data/service/src/main/java/uk/gov/justice/laa/dstew/payments/claimsdata/config/AopConfig.java
@@ -0,0 +1,18 @@
+package uk.gov.justice.laa.dstew.payments.claimsdata.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+/**
+ * Configuration class for setting up Aspect-Oriented Programming (AOP) in the application.
+ * This class enables support for handling components marked with AspectJ's @Aspect annotation.
+ *
+ * <p>It is annotated with @Configuration, indicating that it is a source of bean definitions for the application context.
+ * The @EnableAspectJAutoProxy annotation is used to enable support for proxy-based AOP, allowing
+ * the use of advice, pointcuts, and other AOP features in the application.</p>
+ */
+@Configuration
+@EnableAspectJAutoProxy
+public class AopConfig {
+
+}
