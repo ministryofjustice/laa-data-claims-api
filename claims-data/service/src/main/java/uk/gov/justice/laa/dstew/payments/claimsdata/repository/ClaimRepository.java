2071cc7ed5bde66baefd3c5438ceda104f4ef45b
MapperAtMoJ <maurizio.guerrazzi@justice.gov.uk>
2025-09-10 16:19:01 +0100

    feature/DSTEW-350/implement-claims-search-endpoint - implemented new filter to ger claims based on query params using a Specification<Claim>

--- claims-data/service/src/main/java/uk/gov/justice/laa/dstew/payments/claimsdata/repository/ClaimRepository.java
+++ claims-data/service/src/main/java/uk/gov/justice/laa/dstew/payments/claimsdata/repository/ClaimRepository.java
@@ -4,12 +4,14 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 import uk.gov.justice.laa.dstew.payments.claimsdata.entity.Claim;
 
 /** Repository for managing claim entities linked to submissions. */
 @Repository
-public interface ClaimRepository extends JpaRepository<Claim, UUID> {
+public interface ClaimRepository
+    extends JpaRepository<Claim, UUID>, JpaSpecificationExecutor<Claim> {
   List<Claim> findBySubmissionId(UUID submissionId);
 
   Optional<Claim> findByIdAndSubmissionId(UUID id, UUID submissionId);
