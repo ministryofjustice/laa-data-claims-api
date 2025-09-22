c21c5b5022741ca472e19352e85fc68abd525d2f
NagarjunaRachakonda <Nagarjuna.Rachakonda@justice.gov.uk>
2025-09-22 15:50:03 +0100

    DSTEW-484 Add an integration test for GET bulk submission by ID endpoint

--- claims-data/service/src/integrationTest/java/uk/gov/justice/laa/dstew/payments/claimsdata/controller/BulkSubmissionControllerIntegrationTest.java
+++ claims-data/service/src/integrationTest/java/uk/gov/justice/laa/dstew/payments/claimsdata/controller/BulkSubmissionControllerIntegrationTest.java
@@ -1,10 +1,5 @@
 package uk.gov.justice.laa.dstew.payments.claimsdata.controller;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-import java.util.List;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
@@ -15,29 +10,43 @@ import org.springframework.core.io.ClassPathResource;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
+import org.testcontainers.shaded.com.fasterxml.jackson.databind.ObjectMapper;
 import software.amazon.awssdk.services.sqs.SqsClient;
-import software.amazon.awssdk.services.sqs.model.DeleteMessageRequest;
-import software.amazon.awssdk.services.sqs.model.GetQueueUrlRequest;
-import software.amazon.awssdk.services.sqs.model.GetQueueUrlResponse;
-import software.amazon.awssdk.services.sqs.model.ReceiveMessageRequest;
-import software.amazon.awssdk.services.sqs.model.ReceiveMessageResponse;
+import software.amazon.awssdk.services.sqs.model.*;
 import uk.gov.justice.laa.dstew.payments.claimsdata.entity.BulkSubmission;
 import uk.gov.justice.laa.dstew.payments.claimsdata.model.BulkSubmissionStatus;
+import uk.gov.justice.laa.dstew.payments.claimsdata.model.GetBulkSubmission200Response;
+import uk.gov.justice.laa.dstew.payments.claimsdata.model.GetBulkSubmission200ResponseDetails;
 import uk.gov.justice.laa.dstew.payments.claimsdata.repository.BulkSubmissionRepository;
+import uk.gov.justice.laa.dstew.payments.claimsdata.util.ClaimsDataTestUtil;
+
+import java.time.Instant;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static uk.gov.justice.laa.dstew.payments.claimsdata.util.ClaimsDataTestUtil.USER_ID;
 
 @TestInstance(Lifecycle.PER_CLASS)
 public class BulkSubmissionControllerIntegrationTest extends AbstractIntegrationTest {
 
-  @Autowired private MockMvc mockMvc;
+  @Autowired
+  private MockMvc mockMvc;
 
-  @Autowired private BulkSubmissionRepository bulkSubmissionRepository;
+  @Autowired
+  private BulkSubmissionRepository bulkSubmissionRepository;
 
-  @Autowired private SqsClient sqsClient;
+  @Autowired
+  private SqsClient sqsClient;
 
   private static final String AUTHORIZATION_HEADER = "Authorization";
 
   // must match application-test.yml for test-runner token
   private static final String AUTHORIZATION_TOKEN = "f67f968e-b479-4e61-b66e-f57984931e56";
+
+  private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
 
   @Value("${aws.sqs.queue-name}")
   private String queueName;
@@ -106,4 +115,40 @@ public class BulkSubmissionControllerIntegrationTest extends AbstractIntegration
             .receiptHandle(receiveResp.messages().getFirst().receiptHandle())
             .build());
   }
+
+  @Test
+  void shouldGetBulkSubmissionById() throws Exception {
+    // given: a bulk submission is saved to the database
+    GetBulkSubmission200ResponseDetails bulkSubmission200ResponseDetails = new GetBulkSubmission200ResponseDetails()
+        .addMatterStartsItem(ClaimsDataTestUtil.getBulkSubmissionMatterStart())
+        .addOutcomesItem(ClaimsDataTestUtil.getBulkSubmissionOutcome())
+        .office(ClaimsDataTestUtil.getBulkSubmissionOffice())
+        .schedule(ClaimsDataTestUtil.getBulkSubmissionSchedule());
+    var bulkSubmission =
+        BulkSubmission.builder()
+            .data(bulkSubmission200ResponseDetails)
+            .status(BulkSubmissionStatus.READY_FOR_PARSING)
+            .createdByUserId(USER_ID)
+            .createdOn(Instant.now())
+            .build();
+    BulkSubmission savedBulkSubmission = bulkSubmissionRepository.save(bulkSubmission);
+
+    // when: calling the GET endpoint with the ID
+    MvcResult result =
+        mockMvc
+            .perform(
+                get("/api/v0/bulk-submissions/{id}", savedBulkSubmission.getId().toString())
+                    .header(AUTHORIZATION_HEADER, AUTHORIZATION_TOKEN))
+            .andExpect(status().isOk())
+            .andReturn();
+
+    // then: response body contains bulk_submission_id, status and details
+    String responseBody = result.getResponse().getContentAsString();
+
+    GetBulkSubmission200Response getBulkSubmission200Response = OBJECT_MAPPER.readValue(responseBody, GetBulkSubmission200Response.class);
+    assertThat(getBulkSubmission200Response.getBulkSubmissionId()).isEqualTo(savedBulkSubmission.getId());
+    assertThat(getBulkSubmission200Response.getStatus()).isEqualTo(BulkSubmissionStatus.READY_FOR_PARSING);
+    assertThat(getBulkSubmission200Response.getDetails()).isEqualTo(bulkSubmission200ResponseDetails);
+  }
+
 }
