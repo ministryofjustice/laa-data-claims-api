e0693bf64096b6e0da6a324df46e4312d40e9b10
NagarjunaRachakonda <Nagarjuna.Rachakonda@justice.gov.uk>
2025-10-02 18:56:03 +0100

    DSTEW-499 make case_start_date and case_concluded_date optional

--- claims-data/service/src/main/resources/db/migration/V17__alter_claim_table.sql
+++ claims-data/service/src/main/resources/db/migration/V17__alter_claim_table.sql
@@ -1,3 +1,5 @@
 ALTER TABLE claim
 ALTER COLUMN case_reference_number DROP NOT NULL,
-ALTER COLUMN schedule_reference DROP NOT NULL;
+ALTER COLUMN schedule_reference DROP NOT NULL,
+ALTER COLUMN case_start_date DROP NOT NULL,
+ALTER COLUMN case_concluded_date DROP NOT NULL;
