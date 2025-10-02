42ce2a4ab9f4bf2db6d85fb7b3c61c4ca0739578
vvartakMoJ <vidyanand.vartak@digital.justice.gov.uk>
2025-10-02 16:29:47 +0100

    DSTES-569: Set up logical replication and publication.

--- claims-data/service/src/main/resources/db/migration/V15__setup_logical_replication.sql
+++ claims-data/service/src/main/resources/db/migration/V15__setup_logical_replication.sql
@@ -1,2 +1,24 @@
+CREATE USER ${reporting_username} WITH PASSWORD '${reporting_password}';
 
-CREATE PUBLICATION publication_for_report_service FOR TABLE submission;
+DO $$
+BEGIN
+    IF EXISTS (SELECT 1 FROM pg_roles WHERE rolname = 'rds_replication') THEN
+        EXECUTE 'GRANT rds_replication TO reporting_user';
+END IF;
+END$$;
+
+GRANT USAGE ON SCHEMA claims TO ${reporting_username};
+
+GRANT SELECT ON ALL TABLES IN SCHEMA claims TO ${reporting_username};
+
+ALTER DEFAULT PRIVILEGES IN SCHEMA claims GRANT SELECT ON TABLES TO ${reporting_username};
+
+--Create publications for all tables to be replicated
+CREATE PUBLICATION claims_reporting_service_pub
+    FOR TABLE claims.submission,
+              claims.claim,
+              claims.client,
+              claims.claim_case,
+              claims.claim_summary_fee,
+              claims.calculated_fee_detail;
+
