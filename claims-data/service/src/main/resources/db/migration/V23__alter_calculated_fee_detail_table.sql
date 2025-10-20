f6d2ccfd309f4732fdeff0a6db11bfa5124e7c6b
Jamie Briggs <jamie.briggs@justice.gov.uk>
2025-10-20 15:09:33 +0100

    BC-163 | Fixes to integration tests

--- claims-data/service/src/main/resources/db/migration/V23__alter_calculated_fee_detail_table.sql
+++ claims-data/service/src/main/resources/db/migration/V23__alter_calculated_fee_detail_table.sql
@@ -1,9 +1,9 @@
 /*Rename column to detention_travel_and_waiting_costs_amount*/
-ALTER TABLE claims.calculated_fee_detail
+ALTER TABLE calculated_fee_detail
     RENAME COLUMN detention_and_waiting_costs_amount
         TO detention_travel_and_waiting_costs_amount;
 
 /*Add bolt on substantive hearing count and fee*/
-ALTER TABLE claims.calculated_fee_detail
+ALTER TABLE calculated_fee_detail
     ADD COLUMN bolt_on_substantive_hearing_count INTEGER,
     ADD COLUMN bolt_on_substantive_hearing_fee NUMERIC;
