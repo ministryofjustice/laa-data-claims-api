0be1fae2110ce4a87ce820804d291deec4a4969a
MapperAtMoJ <maurizio.guerrazzi@justice.gov.uk>
2025-10-22 11:21:34 +0100

    Feature/dstew 659/add patch bulk submissions endpoint to claims api (#108)
    Added a new PATCH bulk-submissions end point to update the status of a bulk submission with error code and error description if there's been a parsing or validation error.

--- claims-data/service/src/test/java/uk/gov/justice/laa/dstew/payments/claimsdata/controller/BulkSubmissionControllerTest.java
+++ claims-data/service/src/test/java/uk/gov/justice/laa/dstew/payments/claimsdata/controller/BulkSubmissionControllerTest.java
@@ -3,13 +3,17 @@ package uk.gov.justice.laa.dstew.payments.claimsdata.controller;
 import static java.util.Collections.singletonList;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
 import static uk.gov.justice.laa.dstew.payments.claimsdata.util.ClaimsDataTestUtil.API_URI_PREFIX;
+import static uk.gov.justice.laa.dstew.payments.claimsdata.util.ClaimsDataTestUtil.BULK_SUBMISSION_ID;
 import static uk.gov.justice.laa.dstew.payments.claimsdata.util.ClaimsDataTestUtil.SUBMISSION_ID;
 import static uk.gov.justice.laa.dstew.payments.claimsdata.util.ClaimsDataTestUtil.USER_ID;
 
@@ -23,6 +27,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.springframework.http.MediaType;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.context.ContextConfiguration;
@@ -47,6 +52,7 @@ import uk.gov.justice.laa.dstew.payments.claimsdata.validator.BulkSubmissionFile
 class BulkSubmissionControllerTest {
 
   private static final String BULK_SUBMISSIONS_URI = API_URI_PREFIX + "/bulk-submissions";
+  private static final String BULK_SUBMISSIONS_ID_URI = BULK_SUBMISSIONS_URI + "/{id}";
 
   @InjectMocks private BulkSubmissionController bulkSubmissionController;
 
@@ -171,7 +177,7 @@ class BulkSubmissionControllerTest {
   }
 
   @Nested
-  @DisplayName("GET: " + BULK_SUBMISSIONS_URI + "/{id}")
+  @DisplayName("GET: " + BULK_SUBMISSIONS_ID_URI)
   class GetBulkSubmissionTests {
 
     @Test
@@ -189,6 +195,62 @@ class BulkSubmissionControllerTest {
       when(bulkSubmissionService.getBulkSubmission(id)).thenReturn(expectedResponse);
 
       assertThat(mockMvc.perform(get(BULK_SUBMISSIONS_URI + "/{id}", id))).hasStatus(200);
+    }
+  }
+
+  @Nested
+  @DisplayName("PATCH: " + BULK_SUBMISSIONS_ID_URI)
+  class PatchBulkSubmissionTests {
+
+    @Test
+    @DisplayName("Should return 204 response")
+    void shouldReturn204Response_whenJsonWellFormed() {
+      var body =
+          """
+          {
+            "bulk_submission_id": "ae60657b-89a1-4a33-819d-331a9f5033c7",
+            "status": "VALIDATION_FAILED",
+            "error_code": "V100",
+            "error_description": "This is the error message",
+            "updated_by_user_id": "123"
+          }""";
+
+      // Perform PATCH with body content
+      assertThat(
+              mockMvc
+                  .patch()
+                  .uri(BULK_SUBMISSIONS_ID_URI, BULK_SUBMISSION_ID)
+                  .content(body)
+                  .contentType(MediaType.APPLICATION_JSON)
+                  .accept(MediaType.APPLICATION_JSON))
+          .hasStatus(204);
+
+      verify(bulkSubmissionService).updateBulkSubmission(eq(BULK_SUBMISSION_ID), any());
+    }
+
+    @Test
+    @DisplayName("Should return 400 response")
+    void shouldReturn400Response_whenJsonInWrongFormat() {
+      var body =
+          """
+            "bulk_submission_id": "ae60657b-89a1-4a33-819d-331a9f5033c7",
+            "status": "VALIDATION_FAILED",
+            "error_code": "V100",
+            "error_description": "This is the error message",
+            "updated_by_user_id": "123"
+          }""";
+
+      // Perform PATCH with bad body content
+      assertThat(
+              mockMvc
+                  .patch()
+                  .uri(BULK_SUBMISSIONS_ID_URI, BULK_SUBMISSION_ID)
+                  .content(body)
+                  .contentType(MediaType.APPLICATION_JSON)
+                  .accept(MediaType.APPLICATION_JSON))
+          .hasStatus(400);
+
+      verify(bulkSubmissionService, never()).updateBulkSubmission(eq(BULK_SUBMISSION_ID), any());
     }
   }
 }
