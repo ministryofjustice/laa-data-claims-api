85f616f2872fe424545c30e212399a6147371450
vvartakMoJ <vidyanand.vartak@digital.justice.gov.uk>
2025-10-24 20:30:23 +0100

    Remove unused imports

--- claims-data/service/src/main/java/uk/gov/justice/laa/dstew/payments/claimsdata/service/BulkSubmissionService.java
+++ claims-data/service/src/main/java/uk/gov/justice/laa/dstew/payments/claimsdata/service/BulkSubmissionService.java
@@ -8,7 +8,6 @@ import java.util.UUID;
 import java.util.function.Supplier;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
--- claims-data/service/src/main/java/uk/gov/justice/laa/dstew/payments/claimsdata/service/SubmissionEventPublisherService.java
+++ claims-data/service/src/main/java/uk/gov/justice/laa/dstew/payments/claimsdata/service/SubmissionEventPublisherService.java
@@ -7,7 +7,6 @@ import java.util.Map;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 import software.amazon.awssdk.services.sqs.SqsClient;
 import software.amazon.awssdk.services.sqs.model.GetQueueUrlRequest;
