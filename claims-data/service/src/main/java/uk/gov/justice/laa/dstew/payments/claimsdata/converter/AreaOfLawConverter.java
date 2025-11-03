c66802ffc80e93bb9c3e623faef92e1e4e721e57
MapperAtMoJ <maurizio.guerrazzi@justice.gov.uk>
2025-11-03 15:48:22 +0000

    feature/DSTEW-694/update-area-of-law-field-type-to-enum - added AreaOfLawConverter javadoc comment

--- claims-data/service/src/main/java/uk/gov/justice/laa/dstew/payments/claimsdata/converter/AreaOfLawConverter.java
+++ claims-data/service/src/main/java/uk/gov/justice/laa/dstew/payments/claimsdata/converter/AreaOfLawConverter.java
@@ -4,6 +4,9 @@ import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
 import uk.gov.justice.laa.dstew.payments.claimsdata.model.AreaOfLaw;
 
+/**
+ * This is used to convert the areaOfLaw string into the corresponding {@link AreaOfLaw} enum value.
+ */
 @Component
 public class AreaOfLawConverter implements Converter<String, AreaOfLaw> {
 
