7cadd8833beb6d7a5726df2abe7cf9504ef24cb3
Jose Carlos Arinero Adam <jose.carlosarineroadam@justice.gov.uk>
2025-11-03 17:11:01 +0100

    DSTEW-386 Added Integration Tests for Rate Limiters

--- claims-data/service/src/integrationTest/java/uk/gov/justice/laa/dstew/payments/claimsdata/controller/RateLimiterIntegrationTest.java
+++ claims-data/service/src/integrationTest/java/uk/gov/justice/laa/dstew/payments/claimsdata/controller/RateLimiterIntegrationTest.java
@@ -19,6 +19,10 @@ public class RateLimiterIntegrationTest extends AbstractIntegrationTest {
 
   private static final String GET_ALL_MATTER_STARTS_URI =
       "/submissions/{submissionId}/matter-starts";
+  private static final String GET_SUBMISSION_URI = "/submissions/{submissionId}";
+  private static final String GET_CLAIMS_URI = "/claims";
+  private static final String GET_BULK_SUBMISSION_URI = "/bulk-submissions/{id}";
+  private static final String GET_VALIDATION_MESSAGES_URI = "/validation-messages";
   private Submission submission;
 
   @BeforeEach
@@ -27,21 +31,99 @@ public class RateLimiterIntegrationTest extends AbstractIntegrationTest {
     submission = getSubmissionTestData();
   }
 
-  @DisplayName("Status 200: when a valid submission ID exists in the system")
+  @DisplayName("Status 429 for Matter Start: When Rate Limit exceeded")
   @Test
   @Transactional
-  void getAllMatterStart_shouldReturnOK() throws Exception {
+  void rateLimit_getAllMatterStart() throws Exception {
     for (int i = 0; i < 2; i++) {
-      doRequest(status().isOk());
+      doGetAllMatterStartRequest(status().isOk());
     }
-    doRequest(status().isTooManyRequests());
+    doGetAllMatterStartRequest(status().isTooManyRequests());
   }
 
-  private void doRequest(ResultMatcher resultMatcher) throws Exception {
+  private void doGetAllMatterStartRequest(ResultMatcher resultMatcher) throws Exception {
     mockMvc
         .perform(
             get(API_URI_PREFIX + GET_ALL_MATTER_STARTS_URI, submission.getId())
                 .header(AUTHORIZATION_HEADER, AUTHORIZATION_TOKEN))
+        .andExpect(resultMatcher)
+        .andReturn();
+  }
+
+  @DisplayName("Status 429 for Submission: When Rate Limit exceeded")
+  @Test
+  @Transactional
+  void rateLimit_getSubmission() throws Exception {
+    for (int i = 0; i < 2; i++) {
+      doGetSubmissionRequest(status().isOk());
+    }
+    doGetSubmissionRequest(status().isTooManyRequests());
+  }
+
+  private void doGetSubmissionRequest(ResultMatcher resultMatcher) throws Exception {
+    mockMvc
+        .perform(
+            get(API_URI_PREFIX + GET_SUBMISSION_URI, submission.getId())
+                .header(AUTHORIZATION_HEADER, AUTHORIZATION_TOKEN))
+        .andExpect(resultMatcher)
+        .andReturn();
+  }
+
+  @DisplayName("Status 429 for Get Claims: When Rate Limit exceeded")
+  @Test
+  @Transactional
+  void rateLimit_getClaims() throws Exception {
+    for (int i = 0; i < 2; i++) {
+      doGetClaimsRequest(status().isOk());
+    }
+    doGetClaimsRequest(status().isTooManyRequests());
+  }
+
+  private void doGetClaimsRequest(ResultMatcher resultMatcher) throws Exception {
+    mockMvc
+        .perform(
+            get(API_URI_PREFIX + GET_CLAIMS_URI)
+                .header(AUTHORIZATION_HEADER, AUTHORIZATION_TOKEN)
+                .param("office_code", "OFF_123"))
+        .andExpect(resultMatcher)
+        .andReturn();
+  }
+
+  @DisplayName("Status 429 for Get Bulk Submission: When Rate Limit exceeded")
+  @Test
+  @Transactional
+  void rateLimit_getBulkSubmission() throws Exception {
+    for (int i = 0; i < 2; i++) {
+      doGetBulkSubmissionRequest(status().isOk());
+    }
+    doGetBulkSubmissionRequest(status().isTooManyRequests());
+  }
+
+  private void doGetBulkSubmissionRequest(ResultMatcher resultMatcher) throws Exception {
+    mockMvc
+        .perform(
+            get(API_URI_PREFIX + GET_BULK_SUBMISSION_URI, submission.getBulkSubmissionId())
+                .header(AUTHORIZATION_HEADER, AUTHORIZATION_TOKEN))
+        .andExpect(resultMatcher)
+        .andReturn();
+  }
+
+  @DisplayName("Status 429 for Get Validation messages: When Rate Limit exceeded")
+  @Test
+  @Transactional
+  void rateLimit_getValidationMessages() throws Exception {
+    for (int i = 0; i < 2; i++) {
+      doGetValidationMessagesRequest(status().isOk());
+    }
+    doGetValidationMessagesRequest(status().isTooManyRequests());
+  }
+
+  private void doGetValidationMessagesRequest(ResultMatcher resultMatcher) throws Exception {
+    mockMvc
+        .perform(
+            get(API_URI_PREFIX + GET_VALIDATION_MESSAGES_URI)
+                .header(AUTHORIZATION_HEADER, AUTHORIZATION_TOKEN)
+                .param("submission-id", submission.getId().toString()))
         .andExpect(resultMatcher)
         .andReturn();
   }
