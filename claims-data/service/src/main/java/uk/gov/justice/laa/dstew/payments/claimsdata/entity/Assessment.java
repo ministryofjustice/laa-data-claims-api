5704591ccd5e0d429b685dbe42408b9372e0c79a
jackheslop96 <43777106+jackheslop96@users.noreply.github.com>
2025-11-18 10:49:26 +0000

    BC-227: Linting.

--- claims-data/service/src/main/java/uk/gov/justice/laa/dstew/payments/claimsdata/entity/Assessment.java
+++ claims-data/service/src/main/java/uk/gov/justice/laa/dstew/payments/claimsdata/entity/Assessment.java
@@ -10,6 +10,9 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
+import java.math.BigDecimal;
+import java.time.Instant;
+import java.util.UUID;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -17,10 +20,6 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.UpdateTimestamp;
 import uk.gov.justice.laa.dstew.payments.claimsdata.model.AssessmentOutcome;
-
-import java.math.BigDecimal;
-import java.time.Instant;
-import java.util.UUID;
 
 /** Entity representing an assessment linked to a claim. */
 @Getter
@@ -34,7 +33,8 @@ public class Assessment {
 
   @NotNull
   @Column(nullable = false)
-  @Id private UUID id;
+  @Id
+  private UUID id;
 
   @NotNull
   @ManyToOne(fetch = FetchType.LAZY)
