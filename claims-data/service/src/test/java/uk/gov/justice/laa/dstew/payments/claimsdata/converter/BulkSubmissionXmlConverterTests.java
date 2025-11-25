9034c13017eec135c59baba15d05b417c9ca6f85
NagarjunaRachakonda <Nagarjuna.Rachakonda@justice.gov.uk>
2025-11-25 15:37:08 +0000

    DSTEW-857 Add handling for XML submissions with matter starts containing mediation types or category codes, and update related tests

--- claims-data/service/src/test/java/uk/gov/justice/laa/dstew/payments/claimsdata/converter/BulkSubmissionXmlConverterTests.java
+++ claims-data/service/src/test/java/uk/gov/justice/laa/dstew/payments/claimsdata/converter/BulkSubmissionXmlConverterTests.java
@@ -1,6 +1,9 @@
 package uk.gov.justice.laa.dstew.payments.claimsdata.converter;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.util.ResourceUtils.getFile;
 import static uk.gov.justice.laa.dstew.payments.claimsdata.converter.ConverterTestUtils.getContent;
 import static uk.gov.justice.laa.dstew.payments.claimsdata.converter.ConverterTestUtils.getMultipartFile;
