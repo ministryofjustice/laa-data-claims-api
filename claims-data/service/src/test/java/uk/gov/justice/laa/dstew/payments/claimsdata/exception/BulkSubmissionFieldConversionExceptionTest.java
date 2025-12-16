d5269a6a35d20ac283f1965334c3d4a4817542e4
Dibyatanu Deb <dibyatanu.deb1@justice.gov.uk>
2025-12-16 12:51:16 +0000

    DSTEW-940 added validation message for invalid monetary fields

---
+++ claims-data/service/src/test/java/uk/gov/justice/laa/dstew/payments/claimsdata/exception/BulkSubmissionFieldConversionExceptionTest.java
@@ -0,0 +1,45 @@
+package uk.gov.justice.laa.dstew.payments.claimsdata.exception;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class BulkSubmissionFieldConversionExceptionTest {
+
+  @DisplayName("Exception with out boolean field should return error field and error message")
+  @Test
+  void testBulkSubmissionFieldConversionException() {
+
+    BulkSubmissionFieldConversionException exception =
+        new BulkSubmissionFieldConversionException(
+            "Travel Costs Amount must be a valid monetary value", "travelCosts");
+    Assertions.assertEquals("travelCosts", exception.getRejectedValue());
+    Assertions.assertEquals(
+        "Travel Costs Amount must be a valid monetary value", exception.getMessage());
+  }
+
+  @DisplayName("Exception with  boolean field should return error field and error message")
+  @Test
+  void testBulkSubmissionFieldConversionExceptionBoolean() {
+    BulkSubmissionFieldConversionException exception =
+        new BulkSubmissionFieldConversionException("Youth Court", "truee", true);
+    Assertions.assertEquals("truee", exception.getRejectedValue());
+    Assertions.assertEquals(
+        "Invalid value 'truee' supplied for field 'Youth Court'. Valid values are 'Y' or 'N'",
+        exception.getMessage());
+  }
+
+  @DisplayName(
+      "Exception with out boolean field should return error field and error message and exception")
+  @Test
+  void testBulkSubmissionFieldConversionExceptionNullFieldName() {
+    BulkSubmissionFieldConversionException exception =
+        new BulkSubmissionFieldConversionException(
+            "Travel Costs Amount must be a valid monetary value",
+            "travelCosts",
+            new RuntimeException());
+    Assertions.assertEquals("travelCosts", exception.getRejectedValue());
+    Assertions.assertEquals(
+        "Travel Costs Amount must be a valid monetary value", exception.getMessage());
+  }
+}
