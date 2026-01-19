ed0b5ae02ec4de45ecba60397cd6495fc89be60f
MapperAtMoJ <maurizio.guerrazzi@justice.gov.uk>
2026-01-19 18:15:04 +0000

    poc-javers-1 - add config to turnoff auditing for production environment

---
+++ claims-data/service/src/main/java/uk/gov/justice/laa/dstew/payments/claimsdata/aop/JaversConfig.java
@@ -0,0 +1,20 @@
+package uk.gov.justice.laa.dstew.payments.claimsdata.aop;
+
+import org.javers.core.Javers;
+import org.javers.core.JaversBuilder;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Javers config to turn on/off auditing on.
+ */
+@Configuration
+@ConditionalOnProperty(name = "javers.enabled", havingValue = "true", matchIfMissing = true)
+public class JaversConfig {
+
+  @Bean
+  public Javers javers() {
+    return JaversBuilder.javers().build();
+  }
+}
