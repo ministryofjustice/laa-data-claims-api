bd0d02bb8025be8ae0f92b0fdf9c337c2775b07a
MapperAtMoJ <maurizio.guerrazzi@justice.gov.uk>
2026-01-27 17:59:51 +0000

    poc-javers-2-ignore-things - 2nd change: removed @DiffIgnore from,CalculatedFeeDetail.boltOnDetails and added @DiffIgnore on each field in ValidationMessageLog as class level @DiffIgnore throws JaversException MANAGED_CLASS_MAPPING_ERROR: given javaClass 'class uk.gov.justice.laa.dstew.payments.claimsdata.entity.ValidationMessageLog' is mapped to IgnoredType, expected ManagedType

--- claims-data/service/src/main/java/uk/gov/justice/laa/dstew/payments/claimsdata/entity/ValidationMessageLog.java
+++ claims-data/service/src/main/java/uk/gov/justice/laa/dstew/payments/claimsdata/entity/ValidationMessageLog.java
@@ -14,7 +14,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
-import org.javers.core.metamodel.annotation.DiffIgnore;
+import org.javers.core.metamodel.annotation.ShallowReference;
 import uk.gov.justice.laa.dstew.payments.claimsdata.model.ValidationMessageType;
 
 /** Entity representing a validation message linked to a submission and optionally a claim. */
@@ -24,43 +24,37 @@ import uk.gov.justice.laa.dstew.payments.claimsdata.model.ValidationMessageType;
 @AllArgsConstructor
 @Entity
 @Table(name = "validation_message_log")
+@ShallowReference
 // @DiffIgnore // It’s already a durable log.
 // Using Javers for a log table compounds storage for minimal insight.
 // We could also add @DiffIgnore to every property except maybe the id?
 public class ValidationMessageLog {
 
-  @DiffIgnore @Id private UUID id;
+  @Id private UUID id;
 
-  @DiffIgnore
   @NotNull
   @Column(name = "submission_id", nullable = false)
   private UUID submissionId;
 
-  @DiffIgnore
   @Column(name = "claim_id")
   private UUID claimId;
 
-  @DiffIgnore
   @NotNull
   @Column(nullable = false)
   @Enumerated(EnumType.STRING)
   private ValidationMessageType type;
 
-  @DiffIgnore
   @NotNull
   @Column(nullable = false)
   private String source;
 
-  @DiffIgnore
   @NotNull
   @Column(name = "display_message", nullable = false)
   private String displayMessage;
 
-  @DiffIgnore
   @Column(name = "technical_message")
   private String technicalMessage;
 
-  @DiffIgnore
   @CreationTimestamp
   @Column(name = "created_on", nullable = false, updatable = false)
   private Instant createdOn;
