4422845c813b3c1c275939e3bbdd3e9913609d07
Ben Hobden <ben.hobden@justice.gov.uk>
2026-02-11 19:22:25 +0000

    [bug/BC-546] - Add content type config based off file extension

--- claims-data/service/src/main/java/uk/gov/justice/laa/dstew/payments/claimsdata/validator/BulkSubmissionFileValidator.java
+++ claims-data/service/src/main/java/uk/gov/justice/laa/dstew/payments/claimsdata/validator/BulkSubmissionFileValidator.java
@@ -5,6 +5,8 @@ import org.springframework.web.multipart.MultipartFile;
 import uk.gov.justice.laa.dstew.payments.claimsdata.exception.BulkSubmissionInvalidFileException;
 import uk.gov.justice.laa.dstew.payments.claimsdata.exception.BulkSubmissionValidationException;
 
+import java.util.*;
+
 /**
  * This class is responsible for validating bulk submission files uploaded as part of the submission
  * process.
@@ -15,54 +17,78 @@ import uk.gov.justice.laa.dstew.payments.claimsdata.exception.BulkSubmissionVali
  */
 @Component
 public class BulkSubmissionFileValidator {
+
+  private static final Map<String, Set<String>> ALLOWED_BY_EXT = Map.of(
+          ".csv", Set.of(
+                  "text/csv",
+                  "application/vnd.ms-excel",
+                  "text/plain"
+          ),
+          ".xml", Set.of(
+                  "text/xml",
+                  "application/xml"
+          ),
+          ".txt", Set.of(
+                  "text/plain"
+          )
+  );
+
+  private static final Set<String> SUPPORTED_EXTENSIONS = ALLOWED_BY_EXT.keySet();
+
   /**
    * Validates the provided file against specific criteria, including emptiness, file extension, and
-   * MIME type compliance.
+   * Content type compliance.
    *
    * <p>The validation rules include:
    *
    * <ul>
+   *   <li>Checking the object is a multipart file.
    *   <li>Checking if the file is empty.
-   *   <li>Ensuring the file has a valid extension (.csv or .xml).
-   *   <li>Verifying that the MIME type matches the file extension.
+   *   <li>Ensuring the file has a valid extension (.csv, .xml or .txt).
+   *   <li>Verifying that the Content type matches the file extension.
    * </ul>
    *
    * @param target The object to be validated, expected to be of type {@link MultipartFile}. It
    *     represents the uploaded file which will undergo validation.
    * @throws IllegalArgumentException If the provided file is empty, has an unsupported file
-   *     extension, or if the MIME type is not consistent with the file extension.
+   *     extension, or if the Content type is not consistent with the file extension.
    */
   public void validate(Object target) {
-    MultipartFile file = (MultipartFile) target;
+    // Step 1: validate it is a multipart file
+    if (!(target instanceof MultipartFile file)) {
+      // Causes a 400 Bad Request response to be returned to the client.
+      throw new BulkSubmissionValidationException("The upload is not a MultipartFile");
+    }
 
-    // Step 1: Check if file is null or empty
+    // Step 2: Check if file is null or empty
     if (file.isEmpty()) {
       // Causes a 400 Bad Request response to be returned to the client.
       throw new BulkSubmissionValidationException("The uploaded file is empty");
     }
 
-    // Step 2: Validate file extension
+    // Step 3: Validate file extension
     String originalFilename = file.getOriginalFilename();
-    if (originalFilename == null
-        || (!originalFilename.toLowerCase().endsWith(".csv")
-            && !originalFilename.toLowerCase().endsWith(".xml")
-            && !originalFilename.toLowerCase().endsWith(".txt"))) {
-      // Causes a 415 Unsupported Media Type response to be returned to the client.
-      throw new BulkSubmissionInvalidFileException("Only .csv and .xml files are allowed");
+    final String fileName = originalFilename == null ? "" : originalFilename.toLowerCase().trim();
+    final String extension = getExtensionIfFound(fileName);
+
+    if (extension == null) {
+      // 415 Unsupported Media Type
+      throw new BulkSubmissionInvalidFileException("Only .csv, .xml and .txt files are allowed");
     }
 
-    // Step 3: Validate MIME Type
-    String contentType = file.getContentType();
-    if (originalFilename.toLowerCase().endsWith(".csv") && !"text/csv".equals(contentType)) {
-      // Causes a 415 Unsupported Media Type response to be returned to the client.
+    // Step 4: Validate Content Type
+    final String contentTypeRaw = file.getContentType();
+    final String contentType = contentTypeRaw == null ? "" : contentTypeRaw.toLowerCase().trim();
+    final Set<String> allowed = ALLOWED_BY_EXT.get(extension);
+    if (allowed.stream().noneMatch(a -> a.equalsIgnoreCase(contentType))) {
+      // 415 Unsupported Media Type
       throw new BulkSubmissionInvalidFileException(
-          "Mime type does not match the .csv file extension");
+              "Content type '" + contentType + "' does not match the " + extension + " file extension."
+      );
     }
-    if (originalFilename.toLowerCase().endsWith(".xml")
-        && !("text/xml".equals(contentType) || "application/xml".equals(contentType))) {
-      // Causes a 415 Unsupported Media Type response to be returned to the client.
-      throw new BulkSubmissionInvalidFileException(
-          "Mime type does not match the .xml file extension");
-    }
+  }
+
+  private static String getExtensionIfFound(String fileName) {
+    return SUPPORTED_EXTENSIONS.stream().filter(fileName::endsWith).findFirst().orElse(null);
   }
 }
--- claims-data/service/src/test/java/uk/gov/justice/laa/dstew/payments/claimsdata/validator/BulkClaimFileValidatorTest.java
+++ claims-data/service/src/test/java/uk/gov/justice/laa/dstew/payments/claimsdata/validator/BulkClaimFileValidatorTest.java
@@ -24,6 +24,18 @@ class BulkSubmissionFileValidatorTest {
   }
 
   @Test
+  @DisplayName("Should throw exception if file is not a multipart file")
+  void shouldThrowExceptionIfFileIsNotMultiPart() {
+    // Given an empty file
+    String file = "MockMultipartFile";
+
+    // When / Then
+    assertThatThrownBy(() -> bulkSubmissionFileValidator.validate(file))
+            .isInstanceOf(BulkSubmissionValidationException.class)
+            .hasMessage("The upload is not a MultipartFile");
+  }
+
+  @Test
   @DisplayName("Should throw exception if file is empty")
   void shouldThrowExceptionIfFileIsEmpty() {
     // Given an empty file
@@ -36,7 +48,7 @@ class BulkSubmissionFileValidatorTest {
   }
 
   @ParameterizedTest
-  @ValueSource(strings = {"test.xml", "test.XML"})
+  @ValueSource(strings = {"test.xml", "test.XML", "test.XML "})
   @DisplayName("Should pass validation for valid .xml files")
   void shouldPassValidationForValidXmlFile(String fileName) {
     // Given
@@ -49,13 +61,26 @@ class BulkSubmissionFileValidatorTest {
   }
 
   @ParameterizedTest
-  @ValueSource(strings = {"test.csv", "test.CSV"})
+  @ValueSource(strings = {"test.csv", "test.CSV", "test.CSV "})
   @DisplayName("Should pass validation for valid .csv files")
   void shouldPassValidationForValidCsvFile(String fileName) {
     // Given
     MockMultipartFile file =
         new MockMultipartFile(
             "file", fileName, "text/csv", "col1,col2".getBytes(StandardCharsets.UTF_8));
+
+    // When / Then - No exception is thrown
+    assertThatCode(() -> bulkSubmissionFileValidator.validate(file)).doesNotThrowAnyException();
+  }
+
+  @ParameterizedTest
+  @ValueSource(strings = {"test.txt", "test.TXT", "test.TXT "})
+  @DisplayName("Should pass validation for valid .txt files")
+  void shouldPassValidationForValidTxtFile(String fileName) {
+    // Given
+    MockMultipartFile file =
+            new MockMultipartFile(
+                    "file", fileName, "text/plain", "col1,col2".getBytes(StandardCharsets.UTF_8));
 
     // When / Then - No exception is thrown
     assertThatCode(() -> bulkSubmissionFileValidator.validate(file)).doesNotThrowAnyException();
@@ -73,36 +98,90 @@ class BulkSubmissionFileValidatorTest {
     // When / Then
     assertThatThrownBy(() -> bulkSubmissionFileValidator.validate(file))
         .isInstanceOf(BulkSubmissionInvalidFileException.class)
-        .hasMessage("Only .csv and .xml files are allowed");
+        .hasMessage("Only .csv, .xml and .txt files are allowed");
   }
 
   @ParameterizedTest
-  @ValueSource(strings = {"text/xml", "application/xml", "text/plain"})
-  @DisplayName("Should throw exception if MIME type does not match .csv extension")
-  void shouldThrowExceptionIfMimeDoesNotMatchCsv(String mimeType) {
+  @ValueSource(strings = {"text/xml", "application/xml"})
+  @DisplayName("Should throw exception if Content type does not match .csv extension")
+  void shouldThrowExceptionIfMimeDoesNotMatchCsv(String contentType) {
     // Given
     MockMultipartFile file =
         new MockMultipartFile(
-            "file", "test.csv", mimeType, "col1,col2".getBytes(StandardCharsets.UTF_8));
+            "file", "test.csv", contentType, "col1,col2".getBytes(StandardCharsets.UTF_8));
 
     // When / Then
     assertThatThrownBy(() -> bulkSubmissionFileValidator.validate(file))
         .isInstanceOf(BulkSubmissionInvalidFileException.class)
-        .hasMessage("Mime type does not match the .csv file extension");
+        .hasMessage("Content type '"+contentType+"' does not match the .csv file extension.");
   }
 
   @ParameterizedTest
   @ValueSource(strings = {"text/csv", "text/plain"})
-  @DisplayName("Should throw exception if MIME type does not match .xml extension")
-  void shouldThrowExceptionIfMimeDoesNotMatchXml(String mimeType) {
+  @DisplayName("Should throw exception if Content type does not match .xml extension")
+  void shouldThrowExceptionIfMimeDoesNotMatchXml(String contentType) {
     // Given
     MockMultipartFile file =
         new MockMultipartFile(
-            "file", "test.xml", mimeType, "<p></p>".getBytes(StandardCharsets.UTF_8));
+            "file", "test.xml", contentType, "<p></p>".getBytes(StandardCharsets.UTF_8));
 
     // When / Then
     assertThatThrownBy(() -> bulkSubmissionFileValidator.validate(file))
         .isInstanceOf(BulkSubmissionInvalidFileException.class)
-        .hasMessage("Mime type does not match the .xml file extension");
+        .hasMessage("Content type '"+contentType+"' does not match the .xml file extension.");
+  }
+
+  @ParameterizedTest
+  @ValueSource(strings = {"text/csv", "text/html"})
+  @DisplayName("Should throw exception if Content type does not match .xml extension")
+  void shouldThrowExceptionIfMimeDoesNotMatchTxt(String contentType) {
+    // Given
+    MockMultipartFile file =
+            new MockMultipartFile(
+                    "file", "test.txt", contentType, "<p></p>".getBytes(StandardCharsets.UTF_8));
+
+    // When / Then
+    assertThatThrownBy(() -> bulkSubmissionFileValidator.validate(file))
+            .isInstanceOf(BulkSubmissionInvalidFileException.class)
+            .hasMessage("Content type '"+contentType+"' does not match the .txt file extension.");
+  }
+
+  @ParameterizedTest
+  @ValueSource(strings = {"text/csv", "application/vnd.ms-excel", "text/plain"})
+  @DisplayName("Should pass validation for valid .csv files")
+  void shouldPassValidationForValidCsvFileContentType(String contentType) {
+    // Given
+    MockMultipartFile file =
+            new MockMultipartFile(
+                    "file", "test.csv", contentType, "col1,col2".getBytes(StandardCharsets.UTF_8));
+
+    // When / Then - No exception is thrown
+    assertThatCode(() -> bulkSubmissionFileValidator.validate(file)).doesNotThrowAnyException();
+  }
+
+  @ParameterizedTest
+  @ValueSource(strings = {"text/xml", "application/xml"})
+  @DisplayName("Should pass validation for valid .xml files")
+  void shouldPassValidationForValidXmlFileContentType(String contentType) {
+    // Given
+    MockMultipartFile file =
+            new MockMultipartFile(
+                    "file", "test.xml", contentType, "col1,col2".getBytes(StandardCharsets.UTF_8));
+
+    // When / Then - No exception is thrown
+    assertThatCode(() -> bulkSubmissionFileValidator.validate(file)).doesNotThrowAnyException();
+  }
+
+  @ParameterizedTest
+  @ValueSource(strings = {"text/plain"})
+  @DisplayName("Should pass validation for valid .txt files")
+  void shouldPassValidationForValidTxtFileContentType(String contentType) {
+    // Given
+    MockMultipartFile file =
+            new MockMultipartFile(
+                    "file", "test.txt", contentType, "col1,col2".getBytes(StandardCharsets.UTF_8));
+
+    // When / Then - No exception is thrown
+    assertThatCode(() -> bulkSubmissionFileValidator.validate(file)).doesNotThrowAnyException();
   }
 }
