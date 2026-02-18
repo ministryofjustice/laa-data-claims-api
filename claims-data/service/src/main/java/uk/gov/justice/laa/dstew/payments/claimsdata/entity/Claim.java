157a095cbc0ff494e930150684b3388238369703
Jose Carlos Arinero Adam <jose.carlosarineroadam@justice.gov.uk>
2026-02-18 15:11:01 +0000

    BC-393 Claim Summary Fee relationship is now N to 1

--- claims-data/service/src/main/java/uk/gov/justice/laa/dstew/payments/claimsdata/entity/Claim.java
+++ claims-data/service/src/main/java/uk/gov/justice/laa/dstew/payments/claimsdata/entity/Claim.java
@@ -9,12 +9,14 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import jakarta.persistence.Version;
 import jakarta.validation.constraints.NotNull;
 import java.time.Instant;
 import java.time.LocalDate;
+import java.util.List;
 import java.util.UUID;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -48,8 +50,8 @@ public class Claim {
   @OneToOne(mappedBy = "claim")
   private Client client;
 
-  @OneToOne(mappedBy = "claim")
-  private ClaimSummaryFee claimSummaryFee;
+  @OneToMany(mappedBy = "claim")
+  private List<ClaimSummaryFee> claimSummaryFee;
 
   @OneToOne(mappedBy = "claim")
   private CalculatedFeeDetail calculatedFeeDetail;
