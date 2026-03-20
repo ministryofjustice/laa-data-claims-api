3c85e863217d5390b8e9a9f6528432888aeb0d34
vvartakMoJ <vidyanand.vartak@justice.gov.uk>
2026-03-20 17:10:05 +0000

    DSTEW-1352: WIP: Testing flyway migrations.

---
+++ claims-data/service/src/integrationTest/java/uk/gov/justice/laa/dstew/payments/claimsdata/controller/FlywayDiagnosticTest.java
@@ -0,0 +1,42 @@
+package uk.gov.justice.laa.dstew.payments.claimsdata.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.flywaydb.core.Flyway;
+import org.flywaydb.core.api.MigrationInfo;
+import org.flywaydb.core.api.MigrationInfoService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@Slf4j
+class FlywayDiagnosticTest {
+
+  @Autowired private Flyway flyway;
+
+  @Test
+  void printAllMigrations() {
+    MigrationInfoService info = flyway.info();
+
+    log.info("=== VVVV ALL MIGRATIONS ===");
+    for (MigrationInfo migration : info.all()) {
+      log.info(
+          "Version: %s, Description: %s, State: %s%n",
+          migration.getVersion(), migration.getDescription(), migration.getState());
+    }
+
+    log.info("\n=== VVVV PENDING MIGRATIONS ===");
+    for (MigrationInfo migration : info.pending()) {
+      log.info(
+          "Version: %s, Description: %s%n", migration.getVersion(), migration.getDescription());
+    }
+
+    log.info("\n=== VVVV CURRENT MIGRATION ===");
+    MigrationInfo current = info.current();
+    if (current != null) {
+      log.info("Version: %s, Description: %s%n", current.getVersion(), current.getDescription());
+    } else {
+      log.info("No current migration applied yet.");
+    }
+  }
+}
