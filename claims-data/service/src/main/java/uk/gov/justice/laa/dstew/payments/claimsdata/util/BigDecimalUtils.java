fc9d5f8804f92c16b21e8286be409da2e3ac6d0c
andrewJohnysJustice <andrew.johnys@justice.gov.uk>
2026-04-24 15:34:08 +0100

    [DSTEW-1538] - Fix bug in calculated total amount and ensure two decimal places even when ZERO  (#319)
    ## What
    
    
    https://dsdmoj.atlassian.net/browse/DSTEW-XXX)](https://dsdmoj.atlassian.net/browse/DSTEW-1538
    
    Fixed bug with calculated_total_amount and all values that are rendered
    by calling the BigDecimalUtil - to display 2 decimal places in all cases
    (except null)
    
    ## Checklist
    
    Before you ask people to review this PR please ensure the following and
    mark as complete when done:
    
    - [x] Tests should be passing: `./gradlew test`
    - [x] Github should not be reporting conflicts; you should have recently
    run `git rebase main`.
    - [x] Avoid mixing whitespace changes with code changes in the same
    commit. These make diffs harder to read and conflicts more likely.
    - [x] You should have looked at the diff against main and ensured that
    nothing unexpected is included in your changes.
    - [x] You should have checked that the commit messages say why the
    change was made.
    - [x] If applicable, you have executed the end-to-end (E2E) tests using
    the
    [bulk-submission-and-fee-scheme-tests-](https://github.com/ministryofjustice/bulk-submission-and-fee-scheme-tests-)
    repository and confirmed they pass.

--- claims-data/service/src/main/java/uk/gov/justice/laa/dstew/payments/claimsdata/util/BigDecimalUtils.java
+++ claims-data/service/src/main/java/uk/gov/justice/laa/dstew/payments/claimsdata/util/BigDecimalUtils.java
@@ -38,27 +38,29 @@ public final class BigDecimalUtils {
 
   /**
    * Scales the given {@link BigDecimal} to the specified number of decimal places using {@link
-   * RoundingMode#HALF_UP}, returning {@link BigDecimal#ZERO} when the input is {@code null} or
-   * numerically zero.
+   * RoundingMode#HALF_UP}. If the input is {@code null} or numerically zero, a zero {@code
+   * BigDecimal} scaled to the requested number of decimal places is returned (for example, {@code
+   * 0.00} when {@code scale} is {@code 2}).
    *
    * <p>Behaviour:
    *
    * <ul>
-   *   <li>If {@code amount} is {@code null}, this method returns {@link BigDecimal#ZERO}.
-   *   <li>If {@code amount} is numerically zero (e.g., 0, 0.0, 0.000), this method returns {@link
-   *       BigDecimal#ZERO}.
-   *   <li>Otherwise, the value is scaled to the specified {@code scale} using {@code
-   *       RoundingMode.HALF_UP}.
+   *   <li>If {@code amount} is {@code null}, this method returns a zero value with the given {@code
+   *       scale}.
+   *   <li>If {@code amount} is numerically zero (e.g. {@code 0}, {@code 0.0}, {@code 0.000}), this
+   *       method returns a zero value with the given {@code scale}.
+   *   <li>Otherwise, the value is scaled to {@code scale} decimal places using {@link
+   *       RoundingMode#HALF_UP}.
    * </ul>
    *
    * @param amount the value to scale; may be {@code null}
-   * @param scale the number of decimal places to apply when scaling
-   * @return the scaled {@code BigDecimal}, or {@link BigDecimal#ZERO} if the input is {@code null}
-   *     or numerically zero
+   * @param scale the number of decimal places to apply
+   * @return a {@code BigDecimal} scaled to {@code scale}; zero with the given scale if the input is
+   *     {@code null} or numerically zero
    */
-  public static BigDecimal scaleOrZero(BigDecimal amount, int scale) {
+  public static BigDecimal scaleOrZeroWithScale(BigDecimal amount, int scale) {
     if (amount == null || amount.compareTo(BigDecimal.ZERO) == 0) {
-      return BigDecimal.ZERO;
+      return BigDecimal.ZERO.setScale(scale, RoundingMode.HALF_UP);
     }
     return amount.setScale(scale, RoundingMode.HALF_UP);
   }
