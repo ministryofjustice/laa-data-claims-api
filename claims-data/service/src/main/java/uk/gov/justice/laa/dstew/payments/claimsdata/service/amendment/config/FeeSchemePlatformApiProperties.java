b71cbe7bb7774ff1de8c1ce9baef48dbfa878ca2
Andrew Johnys <Andrew.Johnys@justice.gov.uk>
2026-07-22 13:14:39 +0100

    feat(DSTEW-1595): - Updated config and removed FSP module, using the validator one instead

--- claims-data/service/src/main/java/uk/gov/justice/laa/dstew/payments/claimsdata/service/amendment/config/FeeSchemePlatformApiProperties.java
+++ claims-data/service/src/main/java/uk/gov/justice/laa/dstew/payments/claimsdata/service/amendment/config/FeeSchemePlatformApiProperties.java
@@ -5,7 +5,7 @@ import org.springframework.http.HttpHeaders;
 import uk.gov.justice.laa.dstew.payments.claimsdata.config.ApiProperties;
 
 /** Configuration properties specific to the Fee Scheme Platform API. */
-@ConfigurationProperties(prefix = "laa.dstew.payments.validator.fee-scheme-platform-api")
+@ConfigurationProperties(prefix = "laa.claims.api.amendments.fee-scheme-platform-api")
 public class FeeSchemePlatformApiProperties extends ApiProperties {
 
   public FeeSchemePlatformApiProperties(String url, String accessToken, int readTimeoutMs) {
