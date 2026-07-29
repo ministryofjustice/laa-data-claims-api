13620ec1360efa5afbdcef1d88522a1e03bd742b
Ben Hobden <ben.hobden@justice.gov.uk>
2026-07-29 18:14:08 +0100

    [DSTEW-2058] - Add claim table constraint

--- claims-data/service/src/integrationTest/java/uk/gov/justice/laa/dstew/payments/claimsdata/controller/ClaimControllerIntegrationTest.java
+++ claims-data/service/src/integrationTest/java/uk/gov/justice/laa/dstew/payments/claimsdata/controller/ClaimControllerIntegrationTest.java
@@ -948,7 +948,9 @@ public class ClaimControllerIntegrationTest extends AbstractIntegrationTest {
 
     // Add these mandatory fields to satisfy Bean Validation
     claimA.setMatterTypeCode("TEST-MTC");
-    claimA.setLineNumber(1);
+    // Unique within submission1 (seedClaimsData already uses lines 1,2,4,5) to satisfy
+    // uq_claim_submission_line_number.
+    claimA.setLineNumber(10);
     claimA.setStatus(ClaimStatus.READY_TO_PROCESS);
 
     claimA = claimRepository.saveAndFlush(claimA);
@@ -968,7 +970,7 @@ public class ClaimControllerIntegrationTest extends AbstractIntegrationTest {
 
     // Add these mandatory fields to satisfy Bean Validation
     claimB.setMatterTypeCode("TEST-MTC");
-    claimB.setLineNumber(2);
+    claimB.setLineNumber(11);
     claimB.setStatus(ClaimStatus.READY_TO_PROCESS);
 
     claimB = claimRepository.saveAndFlush(claimB);
