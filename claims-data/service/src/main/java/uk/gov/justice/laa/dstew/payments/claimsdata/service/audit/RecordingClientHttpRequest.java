64a4b9ab301c050a25c809bcad99d6b6c6ad6da3
Daniel Tudor <daniel.tudor@justice.gov.uk>
2026-09-17 14:16:26 +0100

    [DSTEW-2001] - Spotless

--- claims-data/service/src/main/java/uk/gov/justice/laa/dstew/payments/claimsdata/service/audit/RecordingClientHttpRequest.java
+++ claims-data/service/src/main/java/uk/gov/justice/laa/dstew/payments/claimsdata/service/audit/RecordingClientHttpRequest.java
@@ -1,5 +1,7 @@
 package uk.gov.justice.laa.dstew.payments.claimsdata.service.audit;
 
+import java.net.URI;
+import java.util.function.Consumer;
 import org.jspecify.annotations.NullMarked;
 import org.reactivestreams.Publisher;
 import org.springframework.core.io.buffer.DataBuffer;
@@ -11,9 +13,6 @@ import org.springframework.http.client.reactive.AbstractClientHttpRequest;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-import java.net.URI;
-import java.util.function.Consumer;
-
 /**
  * A {@code ClientHttpRequest} that sends nothing and hands every written buffer to the consumer.
  *
@@ -22,68 +21,70 @@ import java.util.function.Consumer;
  */
 @NullMarked
 public class RecordingClientHttpRequest extends AbstractClientHttpRequest {
-    private final HttpMethod method;
-    private final URI uri;
-    private final Consumer<byte[]> sink;
+  private final HttpMethod method;
+  private final URI uri;
+  private final Consumer<byte[]> sink;
 
-    RecordingClientHttpRequest(HttpMethod method, URI uri, Consumer<byte[]> sink) {
-        this.method = method;
-        this.uri = uri;
-        this.sink = sink;
-    }
+  RecordingClientHttpRequest(HttpMethod method, URI uri, Consumer<byte[]> sink) {
+    this.method = method;
+    this.uri = uri;
+    this.sink = sink;
+  }
 
-    @Override
-    public HttpMethod getMethod() {
-        return method;
-    }
+  @Override
+  public HttpMethod getMethod() {
+    return method;
+  }
 
-    @Override
-    public URI getURI() {
-        return uri;
-    }
+  @Override
+  public URI getURI() {
+    return uri;
+  }
 
-    @Override
-    public DataBufferFactory bufferFactory() {
-        return DefaultDataBufferFactory.sharedInstance;
-    }
+  @Override
+  public DataBufferFactory bufferFactory() {
+    return DefaultDataBufferFactory.sharedInstance;
+  }
 
-    @Override
-    public <T> T getNativeRequest() {
-        throw new UnsupportedOperationException("Recording request as no native request");
-    }
+  @Override
+  public <T> T getNativeRequest() {
+    throw new UnsupportedOperationException("Recording request as no native request");
+  }
 
-    @Override
-    public Mono<Void> writeWith(Publisher<? extends DataBuffer> body) {
-        return doCommit(() ->
-                Flux.from(body)
-                        .doOnNext(buffer -> {
-                            try {
-                                byte[] bytes = new byte[buffer.readableByteCount()];
-                                buffer.read(bytes);
-                                sink.accept(bytes);
-                            } finally {
-                                DataBufferUtils.release(buffer);
-                            }
-                        })
-                        .then());
-    }
+  @Override
+  public Mono<Void> writeWith(Publisher<? extends DataBuffer> body) {
+    return doCommit(
+        () ->
+            Flux.from(body)
+                .doOnNext(
+                    buffer -> {
+                      try {
+                        byte[] bytes = new byte[buffer.readableByteCount()];
+                        buffer.read(bytes);
+                        sink.accept(bytes);
+                      } finally {
+                        DataBufferUtils.release(buffer);
+                      }
+                    })
+                .then());
+  }
 
-    @Override
-    public Mono<Void> writeAndFlushWith(Publisher<? extends Publisher<? extends DataBuffer>> body) {
-        return writeWith(Flux.from(body).flatMap(Flux::from));
-    }
+  @Override
+  public Mono<Void> writeAndFlushWith(Publisher<? extends Publisher<? extends DataBuffer>> body) {
+    return writeWith(Flux.from(body).flatMap(Flux::from));
+  }
 
-    @Override
-    public Mono<Void> setComplete() {
-        return doCommit();
-    }
+  @Override
+  public Mono<Void> setComplete() {
+    return doCommit();
+  }
 
-    @Override
-    public void applyHeaders() {}
+  @Override
+  public void applyHeaders() {}
 
-    @Override
-    public void applyCookies() {}
+  @Override
+  public void applyCookies() {}
 
-    @Override
-    public void applyAttributes() {}
+  @Override
+  public void applyAttributes() {}
 }
